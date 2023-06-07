7412804f3ecfbf5879980fab79de26d07b4890ca
Dima <dmytro.jurin@kitu.nau.edu.ua>
2023-06-07 14:47:30 +0300

    use next-analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
+    remotePatterns: [{
         protocol: 'https',
         hostname: 'image.tmdb.org',
       },
@@ -12,15 +14,15 @@ const nextConfig = {
         hostname: 'placehold.co',
       },
     ],
-  },
+  }
 }
 
 const path = require('path');
- 
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
--- src/pages/movies/[id].tsx
+++ src/pages/movies/[id].tsx
@@ -1,24 +1,47 @@
 import React from "react";
+import DetailLayout from "@/layouts/DetailsLayout";
+import MovieDetailsBlock from "@/components/MovieDetailsBlock/MovieDetailsBlock";
+import {
+  getMovieDetails,
+  getRunningQueriesThunk,
+} from "@/redux/api/movies/slice";
+import { wrapper } from "@/redux/store";
 import { MovieDetails } from "@/redux/api/movies/types/MovieDetailsType";
-
-import { GetServerSideProps } from "next";
 
 interface MovieDetailsPageProps {
   id: number;
   data: MovieDetails;
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js');
-  const repo = await res.json();
-  return { props: { data: repo } };
-};
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (context) => {
+    const { id } = context.query;
 
-const MovideDetailsPage: React.FC<MovieDetailsPageProps> = ({ data }) => {
-  
+    if (typeof id === "string") {
+      await store.dispatch(
+        getMovieDetails.initiate({ id, params: "language=uk-UA&page=1" })
+      );
+    }
+
+    const { data } = getMovieDetails.select({
+      id,
+      params: "language=uk-UA&page=1",
+    })(store.getState());
+
+    await Promise.all(store.dispatch(getRunningQueriesThunk()));
+
+    return {
+      props: { id, data },
+    };
+  }
+);
+
+const MovideDetailsPage: React.FC<MovieDetailsPageProps> = ({ id, data }) => {
   return (
     <>
-      <div style={{ color: "#000" }}>{data?.id}</div>
+      <DetailLayout>
+        {data && <MovieDetailsBlock id={id} data={data} />}
+      </DetailLayout>
     </>
   );
 };
