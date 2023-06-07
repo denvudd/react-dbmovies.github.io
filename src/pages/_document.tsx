b3bb5458edc3787f5df49747b907052bf7b6e3ea
Dima <dmytro.jurin@kitu.nau.edu.ua>
2023-06-07 15:11:17 +0300

    try not using ccs-in-js

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,60 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
-
-/* 
-  Issue #16037 "Flash of unstyled content for first load":
-  https://github.com/ant-design/ant-design/issues/16037#issuecomment-483140458 
-  ant-design flash of unstyled content fixed
-*/
-
-type MyDocumentProps = {
-  styles: React.ReactNode
+import { Html, Head, Main, NextScript } from 'next/document';
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export class MyDocument extends Document<MyDocumentProps> {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>{this.props.styles}</Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
-}
-
-MyDocument.getInitialProps = async (ctx) => {
-  const originalRenderPage = ctx.renderPage
-  const cache = createCache()
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) =>
-        function EnhanceApp(props) {
-          return (
-            <StyleProvider cache={cache}>
-              <App {...props} />
-            </StyleProvider>
-          )
-        },
-    })
-
-  const initialProps = await Document.getInitialProps(ctx)
-
-  return {
-    ...initialProps,
-    styles: (
-      <>
-        {initialProps.styles}
-        <style dangerouslySetInnerHTML={{ __html: extractStyle(cache) }} />
-      </>
-    ),
-  }
-}
-
-export default MyDocument;
-
-
-
