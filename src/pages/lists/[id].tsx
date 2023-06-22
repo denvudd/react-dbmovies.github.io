fb0ade5a5f4f549634341e41e62a55df5eb0bdcb
Dima <dmytro.jurin@kitu.nau.edu.ua>
2023-06-22 22:29:03 +0300

    movie details page seo for twitter and og added

--- src/pages/lists/[id].tsx
+++ src/pages/lists/[id].tsx
@@ -19,9 +19,9 @@ import type { ApiError } from "@/redux/api/baseApi/types/ErrorType";
   #1 https://nextjs.org/docs/pages/building-your-application/data-fetching/get-server-side-props#getserversideprops-with-edge-api-routes
   !! Doesn't work in dev mode !!
 */
-export const config = {
-  runtime: 'experimental-edge', // warn: using an experimental edge runtime, the API might change
-}
+// export const config = {
+//   runtime: 'experimental-edge', // warn: using an experimental edge runtime, the API might change
+// }
 
 interface ListDetailsPageProps {
   id: string | string[] | undefined;
--- src/pages/movies/[id]/index.tsx
+++ src/pages/movies/[id]/index.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import DetailLayout from "@/layouts/DetailsLayout";
 import MovieDetailsBlock from "@/components/MovieDetailsBlock/MovieDetailsBlock";
 import Head from "next/head";
+import DetailsTabs from "@/components/UI/DetailsTabs/DetailsTabs";
+import type { ApiError } from "@/redux/api/baseApi/types/ErrorType";
 import type { GetServerSideProps } from "next/types";
 import type { MovieDetails } from "@/redux/api/movies/types";
-import DetailsTabs from "@/components/UI/DetailsTabs/DetailsTabs";
 
 /* 
   The long cold start issue fix
@@ -32,29 +33,69 @@ export const getServerSideProps: GetServerSideProps<{
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=684e3f73d1ca0e692a3016c028aabf72&language=uk-UA`
   );
-  const data: MovieDetails = await res.json();
-  return { props: { data } };
+  const data: MovieDetails | ApiError = await res.json();
+
+  if ("status_code" in data && data.status_code === 34) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/404",
+      },
+    };
+  }
+
+  return { props: { data: data as MovieDetails } };
 };
 
 const MovideDetailsPage: React.FC<MovieDetailsPageProps> = ({ data }) => {
-  const { id } = data;
+  const { id, title, poster_path, backdrop_path, overview, release_date } =
+    data;
 
-  const overview = data && data.overview ? data.overview : undefined;
-  const description = overview
-    ? overview
+  const formattedOverview = data && overview ? overview : undefined;
+  const description = formattedOverview
+    ? formattedOverview
     : "The Movie Database (TMDB) is a popular, user editable database for movies and TV shows.";
+  const posterPath = poster_path
+    ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+    : `https://placehold.co/314x471/png/?text=Not+Found`;
+  const backdropPath = backdrop_path
+    ? `https://image.tmdb.org/t/p/w780/${backdrop_path}`
+    : `https://placehold.co/780x439/png/?text=Not+Found`;
+
   return (
     <>
       <Head>
         <title>
-          {data
-            ? `${data.title} (${data.release_date?.split("-")[0]})`
-            : "Не знайдено"}{" "}
-          — The Movie Database (TMDB)
+          {data &&
+            `${title} (${
+              release_date?.split("-")[0]
+            }) — The Movie Database (TMDB)`}
         </title>
-        <meta name="description" content={description}></meta>
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:image" content={posterPath} />
+        <meta property="og:image" content={backdropPath} />
+        <meta property="og:type" content="video.movie" />
+        <meta property="og:locale" content="uk-UA" />
+        <meta
+          property="og:url"
+          content={`https://react-dbmovies.vercel.app/movies/${id}`}
+        />
+        <meta property="og:site_name" content="The Movie Database Next" />
+        <meta name="twitter:card" content="photo" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={backdropPath} />
+        <meta name="twitter:image:width" content="780" />
+        <meta name="twitter:image:height" content="439" />
+        <meta name="twitter:site" content="@themoviedb" />
+        <meta
+          name="twitter:url"
+          content={`https://react-dbmovies.vercel.app/movies/${id}`}
+        />
       </Head>
-      <DetailsTabs id={data.id}/>
+      <DetailsTabs id={id} />
       <DetailLayout>
         {data && <MovieDetailsBlock id={id} data={data} />}
       </DetailLayout>
